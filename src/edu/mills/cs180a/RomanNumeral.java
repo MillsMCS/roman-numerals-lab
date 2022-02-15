845fb2b29d7bed2996e6f693c3f151ebd2276e0f
Ellen Spertus <spertus@mills.edu>
2022-02-15 08:56:37 -0800

    Add implementation

--- src/edu/mills/cs180a/RomanNumeral.java
+++ src/edu/mills/cs180a/RomanNumeral.java
@@ -1,5 +1,4 @@
 package edu.mills.cs180a;
-
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -13,7 +12,7 @@ public class RomanNumeral {
 	 * The lowest number that can be represented.
 	 */
 	public static final int MIN_VALUE = 1;
-	
+
 	/**
 	 * The highest number that can be represented.
 	 */
@@ -21,40 +20,25 @@ public class RomanNumeral {
 
 	@VisibleForTesting
 	protected static final Map<Character, Integer> LETTERS_TO_VALUES = Map.of(
-		'I', 1,
-		'V', 5,
-		'X', 10,
-		'L', 50,
-		'C', 100,
-		'D', 500,
-		'M', 1000
-	);
-	
-	@VisibleForTesting
-	private static final  Map<String, Integer> SUB_FORM= Map.of(
-			"IV", 4,
-			"IX", 9,
-			"XL", 40,
-			"XC", 90,
-			"CD", 400,
-			"CM", 900
-			
+			'I', 1,
+			'V', 5,
+			'X', 10,
+			'L', 50,
+			'C', 100,
+			'D', 500,
+			'M', 1000
 			);
-			
-	
-	//reverse map keys and values
-	//how to flip code from stackOverflow
-	//https://stackoverflow.com/questions/38589875/swap-keys-with-values-and-vice-versa-of-hashmap
-	@VisibleForTesting
+
+	//reverse key value pair
 	protected static final Map<Integer, Character> VALUES_TO_LETTERS = 
-		LETTERS_TO_VALUES.entrySet().stream()
-						.collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
-			
+			LETTERS_TO_VALUES.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+
+
 	private final int value;
 	private String text;
 
 	/**
-	 * Constructs a newly-allocated {@code RomanNumeral} object that represents 
+	 * Constructs a newly-allocated {@code RomanNumeral} object that represents
 	 * the specified value. The argument must be in the range {@link #MIN_VALUE}
 	 * to {@link #MAX_VALUE}, inclusive.
 	 * 
@@ -86,7 +70,7 @@ public class RomanNumeral {
 					"Value out of bounds [" + MIN_VALUE + "..." + MAX_VALUE + "]: " + value);
 		}
 	}
-	
+
 	/**
 	 * Returns the numeric value of this {@code RomanNumeral}.
 	 * 
@@ -95,114 +79,94 @@ public class RomanNumeral {
 	public int getValue() {
 		return value;
 	}
-	
+
 	@Override
 	public String toString() {
 		return text;
 	}
-	
+
 	@VisibleForTesting
 	protected static int convertFromStringToInt(String s) {
-		String rNum = s.toUpperCase();
-		  if (LETTERS_TO_VALUES.containsKey(rNum.charAt(0)) == false) {
-			  throw new IllegalArgumentException(
-						"Illegal character entered:" + rNum.charAt(0));  
-		  }
-		// if num first is less than second, subtract
-		int total = 0;
-		for (int i = 0; i < rNum.length() ; i++) {			
-			int n = LETTERS_TO_VALUES.get(rNum.charAt(i));
-			if (rNum.length() == 1){ 
-				total = total + n;
-				}
-			if (rNum.length()-1 > i) {
-					int m = LETTERS_TO_VALUES.get(rNum.charAt(i + 1));
-					String pair = rNum.substring(i, i+2);
-						if (SUB_FORM.containsKey(pair)){
-							int newTot = SUB_FORM.get(pair);
-							total = total + newTot;
-							}
-						
-						//case with two equal letters
-						else if (n == m ){
-							
-							total = total + n + m;
-							i++;
-						}
-						else if (rNum.charAt(i) == rNum.length()-1) {
-							total = total + m;
-						}
-						
-			
-	}
+
+		int result = 0;
+		int num = 0;
+
+		s = s.toUpperCase();
+
+		if (!s.matches("[ivxlcdmIVXLCDM]+")) {
+			throw new IllegalArgumentException("String must be roman numeral");  
 		}
-		return total;
+
+		if(s.contains("IIII")||s.contains("VIIII")||s.contains("XXXX")||s.contains("LXXXX")||s.contains("CCCC")||s.contains("DCCCC")) {
+			throw new IllegalArgumentException("Cannot be non-standard forms");  
+		}
+
+		//standard form exceptions
+		if(s.contains("IV")){
+			num=4;
+			result = result + num;
+			s= s.replaceAll(("IV"), "");
+		}
+		if(s.contains("IX")){
+			num=9;
+			result = result + num;
+			s= s.replaceAll(("IX"), "");
+		}
+		if(s.contains("XL")){
+			num=40;
+			result = result + num;
+			s= s.replaceAll(("XL"), "");
+		}
+		if(s.contains("XC")){
+			num=90;
+			result = result + num;
+			s= s.replaceAll(("XC"), "");
+		}
+		if(s.contains("CD")){
+			num=400;
+			result = result + num;
+			s= s.replaceAll(("CD"), "");
+		}
+		if(s.contains("CM")){
+			num=900;
+			result = result + num;
+			s= s.replaceAll(("CM"), "");
+		}
+
+		//map through LETTERS_TO_VALUES to add
+		for (int i=0; i<s.length(); i++) {
+			num = LETTERS_TO_VALUES.get(s.charAt(i));
+			result = result + num;
+		}
+
+		return result;
 	}
-	
+
 	@VisibleForTesting
 	protected static String convertFromIntToString(int n) {
-		//for testing out of bounds numbers
-		if ( n > MAX_VALUE || n < MIN_VALUE) {
-			throw new IllegalArgumentException(
-					"Value out of bounds [" + MIN_VALUE + "..." + MAX_VALUE + "]: " + n);
-		}
-		else {
-			 String output = "";			 
-			 while (n >= 1000) {
-				output = output +  VALUES_TO_LETTERS.get(1000);
-				n = n-1000;
+		if(n>=MIN_VALUE && n<=MAX_VALUE) {
+
+			StringBuilder result = new StringBuilder();
+
+			//arrays for values and symbols that have association
+			int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+			String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+			//loop starting from largest value, compare if given num is subtract-able from value, subtract, repeat
+			int i = 0;
+			while (n > 0) {
+				while ( n >= values[i]) {
+					n = n - values[i];
+					result.append(symbols[i]);
 				}
-			 while (n >= 900) {
-				 n = n - 900;
-				 output = output + "CM";
-			 }
-			 while (n >= 500) {
-					output = output +  VALUES_TO_LETTERS.get(500);
-					n = n-500;
-				 }
-			 while (n >= 400) {
-				 n = n - 400;
-				 output = output + "CD";
-			 }
-			 while (n >= 100) {
-					output = output +  VALUES_TO_LETTERS.get(100);
-					n = n-100;
-				 }
-			 while (n >= 90) {
-				 n = n - 90;
-				 output = output + "XC";
-			 }
-			 while (n >= 50) {
-					output = output +  VALUES_TO_LETTERS.get(50);
-					n = n-50;
-			 }
-			 while (n >= 40) {
-					 n = n - 40;
-					 output = output + "XL";
-				 }
-			 while (n>= 10) {
-				 n = n - 10;
-				 output = output + "X";
-			 }
-			 while (n >= 9) {
-				 n = n - 9;
-				 output = output + "IX";
-			 }
-			 while (n >= 5) {
-					output = output +  VALUES_TO_LETTERS.get(5);
-					n = n-5;
-				 }
-			 while (n >= 4) {
-				 n = n - 4;
-				 output = output + "IV";
-			 }
-			 while (n >= 1) {
-				 n = n-1;
-				 output = output +  VALUES_TO_LETTERS.get(1);
-					
-			 }
-			 //System.out.println("output is " + output); 
-			 return output;
+				i++;
+			}
+			return result.toString();	
+		}
+
+		else {
+			throw new IllegalArgumentException("Out of bound");
+		}
+
 	}
-}
 }
