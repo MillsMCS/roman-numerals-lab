7c1ee32979a703b4e7c46c254c7d8d11c11bff19
Ellen Spertus <spertus@mills.edu>
2022-02-15 08:49:13 -0800

    Add implementation

--- src/edu/mills/cs180a/RomanNumeral.java
+++ src/edu/mills/cs180a/RomanNumeral.java
@@ -83,11 +83,190 @@ public class RomanNumeral {
 	
 	@VisibleForTesting
 	protected static int convertFromStringToInt(String s) {
-		return 0;
+		int returnInt = 0;
+		for (int i = 0; i < s.length(); i++) {
+			switch(s.charAt(i)) {
+			case 'M':
+			case 'm':
+				returnInt += 1000;
+				break;
+			case 'D':
+			case 'd':
+				returnInt += 500;
+				break;
+			case 'C':
+			case 'c':
+				// check for subtractive notation
+				if (i < s.length() - 1) {
+					if(s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'd') {
+						returnInt += 400;
+						i++;
+						break;
+					}
+					if(s.charAt(i + 1) == 'M' || s.charAt(i + 1) == 'm') {
+						returnInt += 900;
+						i++;
+						break;
+					}
+				}
+				returnInt += 100;
+				break;
+			case 'L':
+			case 'l':
+				returnInt += 50;
+				break;
+			case 'X':
+			case 'x':
+				// check for subtractive notation
+				if (i < s.length() - 1) {
+					if(s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'l') {
+						returnInt += 40;
+						i++;
+						break;
+					}
+					if(s.charAt(i + 1) == 'C' || s.charAt(i + 1) == 'c') {
+						returnInt += 90;
+						i++;
+						break;
+					}
+				}
+				returnInt += 10;
+				break;
+			case 'V':
+			case 'v':
+				returnInt += 5;
+				break;
+			case 'I':
+			case 'i':
+				// check for subtractive notation
+				if (i < s.length() - 1) {
+					if(s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'v') {
+						returnInt += 4;
+						i++;
+						break;
+					}
+					if(s.charAt(i + 1) == 'X' || s.charAt(i + 1) == 'x') {
+						returnInt += 9;
+						i++;
+						break;
+					}
+				}
+				returnInt ++;
+				break;
+			default:
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		return returnInt;
 	}
 	
 	@VisibleForTesting
-	protected static String convertFromIntToString(int n) {
-		return null;
+	protected static String convertFromIntToString (int n) {
+		String numeral = "";
+		int remainder = n;
+		while (remainder >= 1) {
+			if (remainder >= 1000) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 1000);
+				remainder = remainder % 1000;
+			}
+			if (remainder >= 900) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 100);
+				remainder = remainder % 100;
+			}
+			if (remainder >= 500) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 500);
+				remainder = remainder % 500;
+			}
+			if (remainder >= 100) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 100);
+				remainder = remainder % 100;
+				}
+			if (remainder >= 90) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 10);
+				remainder = remainder % 10;
+			}
+			if (remainder >= 50) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 50);
+				remainder = remainder % 50;
+			}
+			if (remainder >= 10) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 10);
+				remainder = remainder % 10;
+			}
+			if (remainder >= 9) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 1);
+				remainder = remainder % 1;
+			}
+			if (remainder >= 5) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 5);
+				remainder = remainder % 5;
+			}
+			if (remainder >= 1) {
+				numeral = numeral + convertFromIntToStringHelper(remainder, 1);
+				remainder = remainder % 1;
+			}
+		}
+		return numeral;
+	}
+	
+	// add chars for the given divisor amount
+	private static String convertFromIntToStringHelper (int num, int digitPlace) {
+		int charNum = num / digitPlace;	// # of chars for any one letter
+		String returnString = "";
+		for (int i = 0; i < charNum; i++) {
+			switch (digitPlace) {
+			case 1000:
+				returnString = returnString + "M";
+				break;
+			case 500:
+				returnString = returnString + "D";
+				break;
+			case 100:
+				// check for subtractive case
+				if (charNum == 4) {
+					returnString = returnString + "CD";
+					return returnString;
+				}
+				if (charNum == 9) {
+					returnString = returnString + "CM";
+					return returnString;
+				}
+				returnString = returnString + "C";
+				break;
+			case 50:
+				returnString = returnString + "L";
+				break;
+			case 10:
+				// check for subtractive case
+				if (charNum == 4) {
+					returnString = returnString + "XL";
+					return returnString;
+				}
+				if (charNum == 9) {
+					returnString = returnString + "XC";
+					return returnString;
+				}
+				returnString = returnString + "X";
+				break;
+			case 5:
+				returnString = returnString + "V";
+				break;
+			case 1:
+				// check for subtractive case
+				if (charNum == 4) {
+					returnString = returnString + "IV";
+					return returnString;
+				}
+				if (charNum == 9) {
+					returnString = returnString + "IX";
+					return returnString;
+				}
+				returnString = returnString + "I";
+				break;
+			}
+		}
+		
+		return returnString;
 	}
 }
