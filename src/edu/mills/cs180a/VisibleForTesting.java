6862d3963ada1403f78381c7643703f9d3ef576a
Jada R <jross@mills.edu>
2022-03-31 15:27:21 -0700

    Undo format change

--- src/edu/mills/cs180a/VisibleForTesting.java
+++ src/edu/mills/cs180a/VisibleForTesting.java
@@ -8,6 +8,5 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  * visibility than otherwise needed to enhance testability.
  */
 @Retention(SOURCE)
-public @interface VisibleForTesting {	
-	
+public @interface VisibleForTesting {		
 }
